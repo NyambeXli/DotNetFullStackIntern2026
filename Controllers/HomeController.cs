5b148f81d2643e5d5eef6a95f32baf0af5d1806c
Lindele Xolani Nyambe <lxnyambe@gmail.com>
2026-02-01 13:12:08 +0200

    impliment HomeController to handle default routing
    Added the primary HomeController to manage the initial application entry point. This controller serves the Home/Index view and establishes the base routing logic required for the ASP.NET Core MVC lifecycle.

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CodVeda_FullStack_Intern.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
