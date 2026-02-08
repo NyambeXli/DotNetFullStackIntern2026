35d18a7ce7c0e17d2d840467e088a80bd8ec22ee
Lindele Xolani Nyambe <lxnyambe@gmail.com>
2026-02-08 18:09:28 +0200

    Define User entity and custom
    Implemented the User data model to support system authentication. Included properties for Username, Password (plain-text for current development phase), and UserRole to facilitate Role-Based Access Control (RBAC). This entity serves as the foundation for the Session-based login system.

---
+++ Models/User.cs
@@ -0,0 +1,10 @@
+﻿namespace CodVeda_FullStack_Intern.Models
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string FullName { get; set; } = string.Empty;
+        public string Email { get; set; } = string.Empty;
+        public string Password { get; set; } = string.Empty; 
+    }
+}
